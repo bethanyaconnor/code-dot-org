600a1c1646f0b2492697ea5c593340f9a9f46a18
Bethany Connor <bethany@code.org>
2022-07-21 12:30:29 -0700

    Try running UI tests with saucelabs

---
+++ docker/setup_ui_tests.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# Script for running ui tests within a docker container.
+# In most cases, you will not run this script directly, but instead
+# use docker-compose to run using the ui-tests-compose.yml file in this directory. See instructions in that file.
+
+set -xe
+
+ulimit -n 4096
+
+export CIRCLE_BUILD_NUM=${DRONE_BUILD_NUMBER:-$RANDOM$RANDOM}
+export CIRCLE_NODE_INDEX=1
+export CIRCLE_TEST_REPORTS=/home/circleci/test_reports
+export CIRCLE_ARTIFACTS=/home/circleci/artifacts
+
+mkdir $CIRCLE_ARTIFACTS
+
+# set up locals.yml
+# Need to actually write all the commented out lines also
+set +x
+echo "
+netsim_redis_groups:
+- master: redis://ui-tests-redis:6379
+bundler_use_sudo: false
+properties_encryption_key: $PROPERTIES_ENCRYPTION_KEY
+cloudfront_key_pair_id: $CLOUDFRONT_KEY_PAIR_ID
+cloudfront_private_key: \"$CLOUDFRONT_PRIVATE_KEY\"
+saucelabs_username: $SAUCE_USERNAME
+saucelabs_authkey: $SAUCE_ACCESS_KEY
+ignore_eyes_mismatches: true
+disable_all_eyes_running: true
+firebase_name: $FIREBASE_NAME
+firebase_secret: $FIREBASE_SECRET
+firebase_shared_secret: $FIREBASE_SHARED_SECRET
+use_my_apps: true
+build_dashboard: true
+build_pegasus: true
+build_apps: true
+localize_apps: true
+dashboard_enable_pegasus: true
+dashboard_workers: 5
+skip_seed_all: true
+no_https_store: true
+override_dashboard: \"localhost-studio.code.org\"
+override_pegasus: \"localhost.code.org\"
+dashboard_port: 3000
+pegasus_port: 3000
+animations_s3_directory: animations_circle/$CIRCLE_BUILD_NUM
+assets_s3_directory: assets_circle/$CIRCLE_BUILD_NUM
+files_s3_directory: files_circle/$CIRCLE_BUILD_NUM
+sources_s3_directory: sources_circle/$CIRCLE_BUILD_NUM
+libraries_s3_directory: libraries_circle/$CIRCLE_BUILD_NUM
+dashboard_db_reader: \"mysql://readonly@localhost/dashboard_test\"
+" >> locals.yml
+echo "Wrote secrets from env vars into locals.yml."
+set -x
